480ab7b4114c5c99dcb6bd64bb94204838076182
vohidovich5557 <shokhuk006@gmail.com>
2024-06-23 18:47:45 +0500

    hotfix1

--- src/sections/location/index.tsx
+++ src/sections/location/index.tsx
@@ -11,10 +11,10 @@ export const Location = () => {
                  <MyMap />
             </div>
             <div className=" flex flex-col md:flex-row items-center ml-auto mr-auto justify-center gap-[40px]">
-            <div className="w-auto py-[10px] shadow-md shadow-gray-400/60 bg-white rounded-[20px]  px-[10px]">
+            <div className="w-auto py-[10px] hover:shadow-lg hover:shadow-rareBlack transition-all duration-300 shadow-md shadow-gray-400/60 bg-white rounded-[20px]  px-[10px]">
                 <span><a href="https://maps.app.goo.gl/z8AFy2kH8kRzYNU57" target="_blank">Uzbekistan, Bukhara, Lattico</a></span>
             </div>
-            <div className="w-auto py-[10px] shadow-md shadow-gray-400/60 bg-white rounded-[20px]  px-[10px]">
+            <div className="w-auto py-[10px] hover:shadow-lg hover:shadow-red-600/70 transition-all duration-300 shadow-md shadow-gray-400/60 bg-white rounded-[20px]  px-[10px]">
                 <span className="text-red-600/80"><a href="tel:+998 91 331 55 57">+998 93 331 55 57</a></span>
             </div>
             </div>
