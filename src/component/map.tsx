c7486b976a6faf87b4549276327da8ea25dcfc11
vohidovich5557 <shokhuk006@gmail.com>
2024-06-23 19:06:51 +0500

    locationfix1

--- src/component/map.tsx
+++ src/component/map.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -18,19 +18,65 @@ L.Marker.prototype.options.icon = DefaultIcon;
 const position: [number, number] = [39.7747, 64.4286];
 
 const MyMap: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
+  
+    const handleMouseOver = () => {
+      setShowModal(true);
+    };
+  
+    const handleMouseOut = () => {
+      setShowModal(false);
+    };
+  
     return (
+      <div className="relative w-[100%] h-[100%]">
         <MapContainer center={position} zoom={13} className="w-[100%] h-[100%] rounded-[30px] object-cover">
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={position}>
-                <Popup>
-                    Bukhara, Uzbekistan. <br /> Lattico.
-                </Popup>
-            </Marker>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              Bukhara, Uzbekistan. <br /> Lattico.
+            </Popup>
+          </Marker>
+          <MapComponent setShowModal={setShowModal} />
         </MapContainer>
+        {showModal && (
+          <div className="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex justify-center items-center pointer-events-none">
+            <div className="bg-white p-4 rounded-md">
+              Press Command (Mac) or Control (Windows) to change zoom level
+            </div>
+          </div>
+        )}
+      </div>
     );
-};
+  };
+  
+  const MapComponent: React.FC<{ setShowModal: React.Dispatch<React.SetStateAction<boolean>> }> = ({ setShowModal }) => {
+    const map = useMap();
+  
+    useEffect(() => {
+      map.scrollWheelZoom.disable();
+  
+      const handleMouseOver = () => {
+        setShowModal(true);
+      };
+  
+      const handleMouseOut = () => {
+        setShowModal(false);
+      };
+  
+      map.on('mouseover', handleMouseOver);
+      map.on('mouseout', handleMouseOut);
+  
+      return () => {
+        map.off('mouseover', handleMouseOver);
+        map.off('mouseout', handleMouseOut);
+      };
+    }, [map, setShowModal]);
+  
+    return null;
+  };
 
 export default MyMap;
