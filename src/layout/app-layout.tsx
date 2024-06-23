845bf1a36dc629e66b2c11c631c55a9328493faf
vohidovich5557 <shokhuk006@gmail.com>
2024-06-23 10:21:19 +0500

    hotfix1

--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -1,9 +1,3 @@
-import {Header} from './header'
-import {Footer} from './footer'
-import { Hero } from "../sections/hero";
-import { About } from "../sections/about";
-import { Clients } from '../sections/clients';
-import { TeamCard } from '../sections/team';
 import { memo } from 'react';
 import { Outlet } from 'react-router-dom';
 
